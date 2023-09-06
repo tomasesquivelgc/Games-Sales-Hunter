a9748aa3fcbac01c28dda41dcf6f20cdc6b0d6b1
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-06 17:44:58 -0300

    add arrow svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom';
+import arrowPrevSmall from '../assets/arrowPrevSmall.svg';
 
 const Navbar = () => (
   <div>
     <Link to="/">
-      <button type="button">Home</button>
+      <button type="button">
+        {' '}
+        <img src={arrowPrevSmall} alt="arrow" style={{width:'20px'}}/>
+        {' '}
+      </button>
     </Link>
   </div>
 );
