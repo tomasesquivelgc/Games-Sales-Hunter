a5026a17061df89948b8fb90b4c3e8477ff0d05b
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-06 14:32:32 -0300

    fetchData for viewDeal

--- src/components/ViewDeal.jsx
+++ src/components/ViewDeal.jsx
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ViewDeal = () => {
   let rawID = useParams().title;
   let dealID = encodeURIComponent(rawID);
-  console.log(dealID);
-return (
-<div>
-<h1>ViewDeal</h1>
-</div>
-);
+
+  const [dealData, setDealData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.cheapshark.com/api/1.0/deals?id=${dealID}`
+        );
+        if (!response.ok) {
+          throw new Error("API request failed");
+        }
+        const data = await response.json();
+        setDealData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [dealID]);
+
+  console.log(dealData)
+
+  return (
+    <div>
+      <h1>ViewDeal</h1>
+      {dealData ? (
+        <div>
+          <h2>{dealData.gameInfo.name}</h2>
+          <img src={dealData.gameInfo.thumb} alt={dealData.gameInfo.name} />
+          <p>{dealData.gameInfo.salePrice}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default ViewDeal;
