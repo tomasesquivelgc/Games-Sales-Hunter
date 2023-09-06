1ac8ee5c631a4086a1acddfc0334c11da9a2d20c
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-05 22:46:00 -0300

    create Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
